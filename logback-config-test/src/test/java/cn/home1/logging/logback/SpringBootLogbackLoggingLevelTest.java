b510c7286ae814fc30a2c5746759bc102b840147
sfeng <song.feng@advance.ai>
2018-07-06 19:02:42 +0800

    recover logback-config-test

--- logback-config-test/src/test/java/cn/home1/logging/logback/SpringBootLogbackLoggingLevelTest.java
+++ logback-config-test/src/test/java/cn/home1/logging/logback/SpringBootLogbackLoggingLevelTest.java
@@ -1,66 +1,48 @@
 package cn.home1.logging.logback;
 
-import cn.home1.logging.logback.service.LogService;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import lombok.extern.slf4j.Slf4j;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.runner.RunWith;
-import org.slf4j.event.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.File;
-
-import static org.junit.Assert.*;
-
-
+@ActiveProfiles({"default"})
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = LogbackApplication.class)
+@SpringBootTest(properties = {
+    "debug=false", // debug=false can not disable AUTO-CONFIGURATION REPORT
+    "logging.level.=INFO"
+})
+@Import(LoggingTestApplication.class)
 @Slf4j
 public class SpringBootLogbackLoggingLevelTest {
 
-    @Rule
-    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+  @Rule
+  public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
-    @Autowired
-    LogService logService;
+  @Autowired
+  private LoggingTestApplication application;
 
-    @Autowired
-    Environment environment;
+  @Test
+  public void testLevel() {
+    final String tmpdir = System.getProperty("java.io.tmpdir");
+    log.info("java.io.tmpdir: {}", tmpdir);
 
-    @Test
-    public void testLevel() {
-        final String tmpdir = System.getProperty("java.io.tmpdir");
-        log.info("java.io.tmpdir: {}", tmpdir);
+    final String logFile = tmpdir + "tests.log";
+    log.info("logFile: {}", logFile);
 
-        final String logFile = tmpdir + "tests.log";
-        log.info("logFile: {}", logFile);
+    this.application.infoLog("hello, {}", "world");
+    assertTrue(this.systemOutRule.getLog().contains("hello, world"));
 
-        logService.doLog(null, Level.INFO, "test info");
-        assertFalse(this.systemOutRule.getLog().contains("test info"));
-
-        logService.doLog("org.springframework.test.context.support", Level.INFO, "hello, world");
-        assertTrue(this.systemOutRule.getLog().contains("hello, world"));
-
-        logService.doLog("org.springframework.test.context.support", Level.DEBUG, "hello, debug world");
-        assertFalse(this.systemOutRule.getLog().contains("hello, debug log"));
-
-        logService.doLog("org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor", Level.INFO, "test info");
-        assertFalse(this.systemOutRule.getLog().contains("test info"));
-
-        logService.doLog("org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor", Level.WARN, "test warn");
-        assertTrue(this.systemOutRule.getLog().contains("test warn"));
-    }
-
-    @Test
-    public void testReadSpringProperty() {
-        String applicationName = environment.getProperty("spring.application.name");
-        assertEquals(logService.getLogProperty("application"), applicationName);
-
-        String instanceId = environment.getProperty("eureka.instance.instance-id");
-        assertEquals(logService.getLogProperty("instanceId"), instanceId);
-    }
+    this.application.debugLog("hello, {}", "debug log");
+    assertFalse(this.systemOutRule.getLog().contains("hello, debug log"));
+  }
 }
